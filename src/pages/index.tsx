6a29bebc6bbe4d742c3f1210ccc65873be758c89
COLBY BLANK <colbyblank@gmail.com>
2023-07-05 20:10:48 -0500

    Hero/Count finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className=" prose prose-invert container">
+      <main className="container">
         <Hero />
-        <Preview />
+        <ResumeCount count={0} />
+        <About />
+        <Benefits />
         <Features />
+        <FAQ />
         <CTA />
       </main>
       <Footer />
@@ -34,10 +37,18 @@ const Header: React.FC = () => {
           <p className="">Resume Factory</p>
         </Link>
         <nav className="flex flex-row items-center gap-x-8 sm:gap-x-16">
-          <Link href="#about">About</Link>
-          <Link href="#faq">FAQ</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/api">API</Link>
+          <Link className="transition-colors hover:text-accent" href="#about">
+            About
+          </Link>
+          <Link className="transition-colors hover:text-accent" href="#faq">
+            FAQ
+          </Link>
+          <Link className="transition-colors hover:text-accent" href="/blog">
+            Blog
+          </Link>
+          <Link className="transition-colors hover:text-accent" href="/api">
+            API
+          </Link>
           <div className="flex flex-row gap-x-[20px]">
             <Link
               href="https://github.com/CB-BC/resume-factory"
@@ -49,9 +60,9 @@ const Header: React.FC = () => {
           </div>
           <Link
             href="/"
-            className="rounded bg-accent px-[40px] py-[10px] text-bg shadow"
+            className="rounded border-2 border-accent bg-accent px-[40px] py-[10px] text-bg shadow transition-colors hover:bg-bg hover:text-accent"
           >
-            Sign Up
+            Sign up
           </Link>
         </nav>
       </div>
@@ -61,59 +72,85 @@ const Header: React.FC = () => {
 
 const Hero: React.FC = () => {
   return (
+    <section className="mx-auto my-36 max-w-5xl">
+      <h1 className="mb-12 font-mono text-7xl font-bold">
+        Build tailored resumes without wasting time.
+      </h1>
+      <p className="mb-24 text-2xl leading-10 text-fg-light">
+        Store all your past job experiences and projects in one place. Select
+        the relevant information from your career archive, and we’ll assemble a
+        polished, tailored resume that highlights your strengths and impresses
+        employers.
+      </p>
+      <div className="flex flex-row justify-end">
+        <Link
+          href="/"
+          className="rounded border-2 border-accent bg-accent px-5 py-3 font-mono text-xl text-bg shadow transition-colors hover:bg-bg hover:text-accent"
+        >
+          Get started now
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+const ResumeCount: React.FC<{ count: number }> = ({ count }) => {
+  const bar = <div className="h-[3px] w-full rounded-full bg-fg-light/25 " />;
+
+  return (
+    <section className="mb-[72px] flex flex-row items-center justify-between gap-x-14">
+      {bar}
+      <p className="min-w-max font-mono text-xl font-medium text-fg-light">
+        {count} resumes created so far
+      </p>
+      {bar}
+    </section>
+  );
+};
+
+const About: React.FC = () => {
+  return (
     <section>
-      <h1>Resume Factory</h1>
+      <h2>FEATURES</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eligendi
-        temporibus est sit soluta voluptatem at ex repellat, illo quaerat!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi modi a
+        ratione harum, rerum magni repellendus! Possimus consequatur saepe, odio
+        minus tempore laborum eveniet architecto, pariatur debitis velit
+        obcaecati provident?
       </p>
     </section>
   );
 };
 
-const Preview: React.FC = () => {
+const Benefits: React.FC = () => {
   return (
     <section>
-      <h2>PREVIEW</h2>
+      <h2>FEATURES</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia
-        voluptatem, aut quae, maxime consectetur ipsam nostrum quia quos optio
-        repudiandae ab a! Minima quam hic error soluta saepe aliquam assumenda
-        quasi doloribus distinctio est, aperiam unde accusantium vel. Esse cum
-        consequatur similique sunt officia vero delectus alias. Consequatur quod
-        facere consectetur! Quis dolorem nobis a suscipit sit sint commodi eaque
-        molestiae eum vel ipsum modi quos dolorum, veniam facere tempore illum
-        voluptatibus recusandae, vero sequi quae? Iste aspernatur accusamus
-        eaque? Sapiente consectetur, cumque eaque doloremque numquam alias
-        expedita autem ad. Possimus, atque deserunt nisi ipsam quo
-        exercitationem. Itaque ratione blanditiis aliquid eveniet dolor commodi
-        officia et molestias, consequuntur repellendus vel possimus odit illum
-        voluptate aperiam. Ipsum at sapiente iste eligendi debitis voluptate
-        laborum iure natus suscipit repellat laudantium accusantium, doloremque
-        beatae nemo officiis nam saepe quidem dolorem, earum temporibus
-        deleniti? Ea aspernatur dolore quo soluta sit, rem ullam dolor iure
-        blanditiis laudantium perferendis alias atque a nihil magnam praesentium
-        aperiam quasi. Quibusdam, cumque sed sunt inventore consectetur beatae
-        quos autem ducimus accusantium quia nihil possimus. Est dolore beatae
-        doloremque, tenetur tempora nostrum sapiente eaque voluptas quae aliquam
-        libero fugiat consequatur ex quia natus reprehenderit repellendus atque
-        aut corrupti dolor dignissimos deserunt ducimus. Ipsum natus nemo quia.
-        Cumque, assumenda culpa! Eveniet ut asperiores mollitia, quam iusto
-        voluptas ea culpa nam atque? Repudiandae velit officia, vel temporibus
-        accusamus autem provident perferendis unde officiis. Autem, ex quae
-        explicabo aperiam deleniti vitae nesciunt inventore pariatur itaque
-        eaque nam minima enim in accusantium perferendis excepturi eius beatae
-        facilis voluptatum quos! Deleniti doloribus, excepturi nesciunt corporis
-        esse porro labore, inventore aliquam est harum quasi. Sunt fugiat
-        voluptas magnam praesentium recusandae optio dolor ab enim quis laborum
-        inventore laboriosam doloremque quam repudiandae, incidunt esse corrupti
-        est magni saepe ea neque! Expedita quas deserunt vel culpa dolorem! Eos!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi modi a
+        ratione harum, rerum magni repellendus! Possimus consequatur saepe, odio
+        minus tempore laborum eveniet architecto, pariatur debitis velit
+        obcaecati provident?
       </p>
     </section>
   );
 };
 
 const Features: React.FC = () => {
+  return (
+    <section>
+      <h2>FEATURES</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi modi a
+        ratione harum, rerum magni repellendus! Possimus consequatur saepe, odio
+        minus tempore laborum eveniet architecto, pariatur debitis velit
+        obcaecati provident?
+      </p>
+    </section>
+  );
+};
+
+const FAQ: React.FC = () => {
   return (
     <section>
       <h2>FEATURES</h2>
