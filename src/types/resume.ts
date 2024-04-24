c82da9da538f63f3f25cda02994eb4fce7f26be0
COLBY BLANK <colbyblank@gmail.com>
2024-04-24 05:02:36 -0500

    Fixed index key error in footercolumn, beginning build of data submission

--- src/types/resume.ts
+++ src/types/resume.ts
@@ -1,24 +1,23 @@
 // objects = typescript types
 
 interface Education {
-    degree: string;
-    year: number;
-    school: string;
+  degree: string;
+  year: number;
+  school: string;
 }
 
 interface Project {
-    link: string[];
-    title: string;
-    description: string;
+  link: string[];
+  title: string;
+  description: string;
 }
 
-
 export interface Resume {
-    name: string;
-    number: number;
-    email: string;
-    links: string[];
-    education: Education[];
-    projects: Project[];
-    skills: string[];
+  name: string;
+  number: string;
+  email: string;
+  links: string[];
+  education: Education[];
+  projects: Project[];
+  skills: string[];
 }
